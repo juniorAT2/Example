938f12546c5e785d5544081bb7552825098a1581
juniorAT2 <alessio.tutera.95@gmail.com>
2022-09-06 18:25:40 +0200

    First commit

--- src/main/java/it/accenture/controller/PeopleController.java
+++ src/main/java/it/accenture/controller/PeopleController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/person")
 public class PeopleController {
+
     private AbstractPeopleService aps;
     @Autowired
     public PeopleController (AbstractPeopleService aps) {
--- src/main/java/it/accenture/model/People.java
+++ src/main/java/it/accenture/model/People.java
@@ -23,5 +23,4 @@ public class People {
     private String surname;
     @Column(name = "DOB")
     private LocalDate dob;
-
 }
