5bdf2049343ca9d3a3734f3506f5abcc7b93a549
juniorAT2 <alessio.tutera.95@gmail.com>
2022-09-06 16:41:34 +0200

    First commit

---
+++ src/test/java/it/accenture/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.accenture.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
