8a6b7562d88924dd1646ed9f5844305f88ac0596
juniorAT2 <alessio.tutera.95@gmail.com>
2022-09-08 14:58:44 +0200

    remove service.abstraction

--- src/main/java/it/accenture/services/implementations/CrudService.java
+++ src/main/java/it/accenture/services/implementations/CrudService.java
@@ -2,12 +2,11 @@ package it.accenture.services.implementations;
 
 import it.accenture.exceptions.EntityNotFoundException;
 import it.accenture.model.abstractions.WithId;
-import it.accenture.services.abstractions.AbstractCrudService;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public class CrudService<T extends WithId<K>, K, R extends CrudRepository<T, K>> implements AbstractCrudService<T, K> {
+public class CrudService<T extends WithId<K>, K, R extends CrudRepository<T, K>>  {
     protected final R repo;
     protected Class<?> entityClass;
     public static final String ERROR_NOT_FOUND = "L'entità %s con id %d non esiste";
@@ -16,17 +15,14 @@ public class CrudService<T extends WithId<K>, K, R extends CrudRepository<T, K>>
         this.entityClass = entityClass;
     }
 
-    @Override
     public Iterable<T> getAll() {
         return repo.findAll();
     }
 
-    @Override
     public Optional<T> findById(K k) {
         return repo.findById(k);
     }
 
-    @Override
     public void deleteById(K k) throws EntityNotFoundException {
         if (exists(k)) {
             repo.deleteById(k);
@@ -35,7 +31,6 @@ public class CrudService<T extends WithId<K>, K, R extends CrudRepository<T, K>>
         }
     }
 
-    @Override
     public T saveOrUpdate(T t) throws EntityNotFoundException {
         if (t.getId()!=null && !t.getId().equals(0) && !exists(t.getId())) {
             throw new EntityNotFoundException(ERROR_NOT_FOUND, entityClass, (long) t.getId());
@@ -43,7 +38,6 @@ public class CrudService<T extends WithId<K>, K, R extends CrudRepository<T, K>>
         return repo.save(t);
     }
 
-    @Override
     public boolean exists(K id) {
         return findById(id).isPresent();
     }
